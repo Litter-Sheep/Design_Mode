a619c28a7a8e7db9674cb2be94e7af9de6aeface
xiaobai <836056071@qq.com>
2022-08-10 13:43:55 +0800

    Java 字节笔试算法

--- src/main/java/com/xiaobai/interviews/DailyNews1.java
+++ src/main/java/com/xiaobai/interviews/DailyNews1.java
@@ -7,9 +7,7 @@ package com.xiaobai.interviews;
  * @createDate: 2022-08-05
  */
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  *
@@ -18,37 +16,68 @@ import java.util.Scanner;
  */
 
 public class DailyNews1 {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
             int n = scanner.nextInt();
-            String[] strings = new String[n];
+            String[] strs = new String[n];
             for (int i = 0; i < n; i++) {
-                strings[i] = scanner.next();
+                strs[i] = scanner.next();
             }
+            System.out.println(maxSum(n, strs));
         }
     }
 
-    public static long maxSum(int n, String string) {
-        long sum = 0;       // 返回最大的和
-        HashMap<Character, Long> hashMap = new HashMap<Character, Long>();
-        ArrayList<Character> arrayList = new ArrayList<Character>();
+    // 返回最大和
+    public static long maxSum(int n, String[] strs) {
+        // 和
+        long sum = 0;
+        HashMap<Character, Long> map = new HashMap<Character, Long>();
+        ArrayList<Character> headList = new ArrayList<Character>();
         for (int i = 0; i < n; i++) {
-            setWeight(string[i], hashMap, arrayList);
+            setWeight(strs[i], map, headList);
         }
 
-    }
-
-    // 设置每个字符的权重，并保存首字符
-    public static void setWeight(String string, HashMap<Character, Long> hashMap, ArrayList<Character> arrayList) {
-        char[] chars = string.toCharArray();
-        long init = 1;
-        for (int i = chars.length - 1; i >= 0; i--) {
-            // 检查hashMap中，key为char[i]是否存在
-            if (hashMap.containsKey(chars[i])) {
-
+        // 按照各个字符的权重进行排序
+        ArrayList<Map.Entry<Character, Long>> list = new ArrayList<Map.Entry<Character, Long>>(map.entrySet());     // 将键值对应关系存入数组中
+        Collections.sort(list, new Comparator<Map.Entry<Character, Long>>() {
+            @Override
+            public int compare(Map.Entry<Character, Long> o1, Map.Entry<Character, Long> o2) {
+                // 根据字符的权重进行从小到大排序
+                return o1.getValue() > o2.getValue() ? -1 : 1;
+            }
+        });
+        int number = 9;         // 最大的数字
+        // 除去头元素为0的情况
+        // 办法：将权重最小的且不是头元素的第一个字符放置在映射值为0的位置
+        if (list.size() == 10) {
+            for (int i = 9; i >= 0; i--) {
+                if (!headList.contains(list.get(i).getKey())) {
+                    list.remove(i);
+                    break;
+                }
             }
         }
+        for (Map.Entry<Character, Long> entry : list) {
+            sum += entry.getValue() * number;
+            number--;
+        }
+        return sum;
+    }
 
+    // 设置每个字符串中每个字符的权重，并保存首字符
+    public static void setWeight(String string, HashMap<Character, Long> map, ArrayList<Character> headList) {
+        char[] cs = string.toCharArray();
+        long init = 1;
+        for (int i = cs.length - 1; i >= 0; i--) {
+            if (map.containsKey(cs[i])) {
+                map.put(cs[i], map.get(cs[i]) + init);
+            } else {
+                map.put(cs[i], init);
+            }
+            init *= 10;
+        }
+        headList.add(cs[0]);
     }
 }
